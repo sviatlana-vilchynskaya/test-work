01c1c398b7d6d5357e8103a546f1b4570ec20883
Sviatlana Vilchynskaya <kohechka1993@gmail.com>
2023-08-04 14:25:58 +0300

    fix: webpack.config.js fix image processing and build in dist.

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,64 @@
 /* eslint-disable import/no-extraneous-dependencies */
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { HotModuleReplacementPlugin } = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
+module.exports = (env, argv) => {
+  const isDevelopment = argv.mode === 'development';
+  const publicPath = isDevelopment ? '/' : './';
+
+
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, isDevelopment ? 'src' : 'dist'),
+      filename: 'bundle.js',
+      publicPath: publicPath, 
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
           },
         },
-      },
-      {
-        test: /.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /.(png|jpe?g|gif|svg)$/i,
-        use: [
+        {
+          test: /\.scss$/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'assets/images/[name][ext]',
+          },
+        },
+
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        publicPath: publicPath,
+      }),
+      isDevelopment && new HotModuleReplacementPlugin(),
+      !isDevelopment && new CopyWebpackPlugin({
+        patterns: [
           {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'images',
-            },
+            from: 'src/assets/images',
+            to: 'assets/images',
           },
         ],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'style.css',
-    }),
-  ],
+      }),
+    ].filter(Boolean),
+    devServer: {
+      static: path.join(__dirname, 'src'),
+      hot: isDevelopment,
+      port: 8080,
+    },
+  };
 };
